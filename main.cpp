7e7797eba76137198917c7d7bcc52c6e26c017cb
Wolfe <awolfe@ea.com>
2024-10-03 16:55:20 -0700

    Writing blog post next

--- main.cpp
+++ main.cpp
@@ -12,14 +12,20 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 typedef unsigned int uint;
 typedef std::array<uint, 2> uint2;
+typedef std::array<float, 2> float2;
 
 // -1 means non deterministic
 #define SEED() 435
 
 #define NUM_FRAMES() 10  // 0 to save all frames
 static const uint2 c_imageSizes2D[] = { { 64, 64 }, { 128, 128 }, { 256, 256 }, { 512, 512 }, { 1024, 1024 }, { 2048, 2048 } };
+
+static const uint c_numIntegrationtests = 1000;
 
 // R2 is from http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/
 static const float c_R2_g = 1.32471795724474602596f;
@@ -35,6 +41,16 @@ T Clamp(T value, T themin, T themax)
 		return themax;
 	else
 		return value;
+}
+
+float Lerp(float A, float B, float t)
+{
+	return A * (1.0f - t) + B * t;
+}
+
+float Frac(float v)
+{
+	return v - std::floor(v);
 }
 
 // From https://blog.demofox.org/2015/01/24/programmatically-calculating-gcd-and-lcm/
@@ -127,9 +143,10 @@ void DoTest2D_SingleTest(const char* testName, const uint2& dims, const LAMBDA& 
 		}
 		else
 		{
-			saveFrame |= (index == 0); // Save the first frame
-			saveFrame |= (index == dims[0] * dims[1] - 1); // Save the last frame
+			// Save the last frame
+			saveFrame |= (index == dims[0] * dims[1] - 1); 
 
+			// save frames periodically
 			if (index > 0)
 			{
 				int lastSection = (index - 1) * NUM_FRAMES() / (dims[0] * dims[1]);
@@ -215,15 +232,38 @@ void DoTest2D(const uint2& dims, uint seed)
 		);
 	}
 
+	// R2 sequence - V1
+	// Do the normal R2 sequence, but convert it to integers
+	{
+		std::mt19937 rng(seed);
+		std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+		float2 point = { dist(rng), dist(rng) };
+
+		DoTest2D_SingleTest("R2v1", dims,
+			[&point, &dims](uint index)
+			{
+				uint2 pointi;
+				pointi[0] = Clamp<uint>(uint(point[0] * float(dims[0]) + 0.5f), 0, dims[0] - 1);
+				pointi[1] = Clamp<uint>(uint(point[1] * float(dims[1]) + 0.5f), 0, dims[1] - 1);
+
+				point[0] = Frac(point[0] + c_R2_a1);
+				point[1] = Frac(point[1] + c_R2_a2);
+
+				return pointi;
+			}
+		);
+	}
+
 	/*
-	// R2 sequence - Naive
+	// R2 sequence - V2
 	// Run a shuffle for each axis, using the R2 sequence
 	// Total failure - the shuffles repeat every dims[].
 	{
 		LDShuffle shuffle1(dims[0], seed, c_R2_a1);
 		LDShuffle shuffle2(dims[1], seed, c_R2_a2);
 
-		DoTest2D_SingleTest("R2Naive", dims,
+		DoTest2D_SingleTest("R2v2", dims,
 			[&shuffle1, &shuffle2](uint index)
 			{
 				uint x = shuffle1.GetValueAtIndex(index);
@@ -235,7 +275,7 @@ void DoTest2D(const uint2& dims, uint seed)
 	*/
 
 	/*
-	// R2 sequence - Better
+	// R2 sequence - V3
 	// Convert the 2d R2 constants into a 1D offset index.
 	// Run a 1D shuffle based on that, and use that 1D index as the 2d point on the image without transformation.
 	// Very streaky unfortunately.
@@ -251,7 +291,7 @@ void DoTest2D(const uint2& dims, uint seed)
 
 		LDShuffle shuffle(dims[0] * dims[1], seed, coprime);
 
-		DoTest2D_SingleTest("R2Better", dims,
+		DoTest2D_SingleTest("R2v3", dims,
 			[&shuffle, &dims](uint index)
 			{
 				uint shuffledIndex = shuffle.GetValueAtIndex(index);
@@ -267,10 +307,103 @@ void DoTest2D(const uint2& dims, uint seed)
 	// One thing I tried was to do a 1D shuffle for width to get all X values exactly once.
 	// While doing that, I also did a 1D shuffle for height to get all the Y values exactly once.
 	// At the end of this, there was one pixel per row.
-	// Now, repeat that, but with an offset to X, using another 1D LDS shuffle.
+	// Now, repeat that, but with an offset to X, using another 1D LDS shuffle over width.
+	// It gives the same point set, but being offset it was hard to tell and was decently spaced.
 	// I also had to make sure the various numbers were coprime to each other, and the dimensions of the texture.
 	// I also had to use 3 irrationals: golden ratio, sqrt(2), sqrt(3).
 	// This worked pretty decently at some screen resolutions but not others.
+}
+
+void DoIntegrationTest(uint seed)
+{
+	int w, h, c;
+	unsigned char* pixels = stbi_load("cabin.png", &w, &h, &c, 1);
+
+	// calculate the actual average value of the image
+	float average = 0.0f;
+	for (size_t i = 0; i < 512 * 512; ++i)
+		average = Lerp(average, float(pixels[i]) / 255.0f, 1.0f / float(i + 1));
+	//printf("Actual average: %f\n", average);
+
+	printf("Integrating....\n");
+
+	std::mt19937 rng(seed);
+
+	std::vector<float> mse[3];
+	mse[0].resize(512 * 512, 0.0f);
+	mse[1].resize(512 * 512, 0.0f);
+	mse[2].resize(512 * 512, 0.0f);
+
+	int lastPercent = -1;
+	for (uint testIndex = 0; testIndex < c_numIntegrationtests; ++testIndex)
+	{
+		int percent = int(100.0f * float(testIndex) / float(c_numIntegrationtests - 1));
+		if (percent != lastPercent)
+		{
+			lastPercent = percent;
+			printf("\r%i%%", percent);
+		}
+
+		// Make a shuffled list of pixels to get a white noise ordering
+		std::vector<uint> shuffleOrder(512 * 512);
+		{
+			for (uint i = 0; i < shuffleOrder.size(); ++i)
+				shuffleOrder[i] = i;
+			std::shuffle(shuffleOrder.begin(), shuffleOrder.end(), rng);
+		}
+
+		// One low discrepancy shuffler will be used by both the Hilbert and ZOrder versions
+		LDShuffle shuffle(512 * 512, rng());
+
+		float avgs[3] = { 0.0f, 0.0f, 0.0f };
+		for (size_t sampleIndex = 0; sampleIndex < 512 * 512; ++sampleIndex)
+		{
+			// white noise
+			avgs[0] = Lerp(avgs[0], float(pixels[shuffleOrder[sampleIndex]]) / 255.0f, 1.0f / float(sampleIndex + 1));
+
+			// Get the shuffled index
+			uint shuffledIndex = shuffle.GetValueAtIndex((uint)sampleIndex);
+
+			// Hilbert
+			{
+				int x, y;
+				Hilbert::d2xy(512 * 512, shuffledIndex, &x, &y);
+
+				uint readIndex = y * 512 + x;
+				avgs[1] = Lerp(avgs[1], float(pixels[readIndex]) / 255.0f, 1.0f / float(sampleIndex + 1));
+			}
+
+			// ZOrder
+			{
+				uint x, y;
+				ZOrder::OneDToTwoD(shuffledIndex, x, y);
+
+				uint readIndex = y * 512 + x;
+				avgs[2] = Lerp(avgs[2], float(pixels[readIndex]) / 255.0f, 1.0f / float(sampleIndex + 1));
+			}
+
+			// Keep track of MSE across tests
+			for (int noiseIndex = 0; noiseIndex < 3; ++noiseIndex)
+			{
+				float error = std::abs(avgs[noiseIndex] - average);
+				mse[noiseIndex][sampleIndex] = Lerp(mse[noiseIndex][sampleIndex], error * error, 1.0f / float(testIndex + 1));
+			}
+		}
+	}
+	printf("\r100%%");
+
+	// Write the data out
+	FILE* file = nullptr;
+	fopen_s(&file, "out/_integration.csv", "wb");
+	fprintf(file, "\"Index\",\"White\",\"Hilbert\",\"ZOrder\"\n");
+	for (uint sampleIndex = 0; sampleIndex < 512 * 512; sampleIndex += 512)
+	{
+		fprintf(file, "\"%u\",\"%f\",\"%f\",\"%f\"\n", sampleIndex+1, std::sqrt(mse[0][sampleIndex]), std::sqrt(mse[1][sampleIndex]), std::sqrt(mse[2][sampleIndex]));
+	}
+	fclose(file);
+
+
+	stbi_image_free(pixels);
 }
 
 int main(int argc, char** argv)
@@ -283,7 +416,12 @@ int main(int argc, char** argv)
 	printf("Seed = %u\n\n", seed);
 
 	for (const uint2& size : c_imageSizes2D)
+	{
+		printf("============= 2D Tests %u x %u =============\n\n", size[0], size[1]);
 		DoTest2D(size, seed);
+	}
+
+	DoIntegrationTest(seed);
 
 	return 0;
 }
@@ -291,16 +429,13 @@ int main(int argc, char** argv)
 
 /*
 TODO:
-- could also add r2 back in.
-- could also try z order. You just interleave bits: https://en.wikipedia.org/wiki/Z-order_curve
- - can re-centralize tests. make it easier for people to test their own?
-- why does hilbert 1024 look so strange, with clumps? 4096 looks ok. 2048 looks strange too. maybe just note it for now and move on.
- - hilbert is such that nearby 1d points are nearby in 2d. The converse may not be true, that distance 1d points are always distant in 2D?
-- should we integrate an image and graph white noise (shuffled) vs this?  maybe with multiple seeds
-- show how to invert it.
-- write blog post
- - include DFT (from gigi? or python snippets. whatever)
+
+ Blog:
+ - show how converting R2 to int has a lot of overlaps
  - needs to be a power of 2, how you wrote it. would need to adapt the hilbert code for non power of 2
  - or break the image into powers of 2 and shuffle each individually, but use a weighted round robin to visit each? (could link to low discrepancy weighted round robin)
-
+  * Motivation - when you want to sample something without repeats, possibly exhaustively, but get 'good sampling' characteristics
+ - why does hilbert 1024 look so strange, with clumps? 4096 looks ok. 2048 looks strange too. maybe just note it for now and move on.
+  - hilbert is such that nearby 1d points are nearby in 2d. The converse may not be true, that distance 1d points are always distant in 2D?
+ - show how to invert it.
 */
